81cbeb0fe75628938002e7247141e41a5a9ca3ea
Andres <andrest2455@gmail.com>
2021-01-25 16:03:27 -0800

    Began adding error handling for file data.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from win10toast import ToastNotifier
-from datetime import datetime
 from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from datetime import datetime
 import schedule
 import time
 import sys
 import os
 import logging
+
+"""
+To do:
+- Error handle the input file data
+- Fix program exiting if notification time is invalid
+"""
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -15,33 +22,16 @@ class MyHandler(FileSystemEventHandler):
 def notification(people):
     names = []
     today = datetime.today().strftime("%m/%d")
-    if(str.startswith(today, "0")):
+    if(today.startswith("0")):
         today = today[1:]
-    print(today)
     for person in people:
         if(person[1] == today):
             names.append(person[0])
-    print(names)
-    if(len(names) != 0):
+    if(len(names) > 0):
         toast = ToastNotifier()
         toast.show_toast("Birthday Reminder", "It is {}'s brithday today!".format(", ".join(names)), duration=20)
 
 def main():
-    people = []
-
-    if(os.path.isfile(sys.argv[1]) and sys.argv[1].endswith('.txt')):
-        f = open(sys.argv[1], "r")
-        lines = f.readlines()
-        for line in lines:
-            person = line.rstrip().split(' ')
-            people.append(person)
-        print(people)
-    else:
-        print("File is not valid! Only .txt files!")
-        return
-
-    schedule.every().day.at("09:00").do(notification, people)
-
     path = os.getcwd()
 
     observer = Observer()
@@ -49,12 +39,36 @@ def main():
     observer.schedule(event_handler, path, recursive=False)
     observer.start()
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    print("Birthday notification system is active.")
+    people = []
+    if(os.path.isfile(sys.argv[1]) and sys.argv[1].endswith('.txt')):
+        f = open(sys.argv[1], "r")
+        lines = f.readlines()
+        notifyTime = lines[0].rstrip()
+        lines.pop(0)
+        try:
+            print(datetime.strptime(notifyTime, '%H:%M'))
+        except:
+            print("Invalid time for notification.")
+             
+        for line in lines:
+            person = line.rstrip().split(' ')
+            people.append(person)
+    else:
+        print("File is not valid! Only .txt files!")
+        return
 
-    observer.stop()
-    observer.join()
+    try:
+        schedule.every().day.at(str(notifyTime)).do(notification, people)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except:
+        print("Scheduler not working.")
+    finally:
+        observer.stop()
+        observer.join()
 
 if __name__ == "__main__":
     main()
